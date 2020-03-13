ab2d4a7f1c04f69c42398c0151e8b396b2f331d8
Santiago Del Rey Juárez <santiago.del.rey@est.fib.upc.edu>
2020-03-13 03:56:22 +0100

    Solve IDE compatibility problem and add orchestator to gradle

--- app/src/androidTest/java/org/pesmypetcare/mypetcare/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/pesmypetcare/mypetcare/ExampleInstrumentedTest.java
@@ -2,13 +2,13 @@ package org.pesmypetcare.mypetcare;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,6 +22,7 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("org.pesmypetcare.mypetcare", appContext.getPackageName());
+        assertEquals("Expected org.pesmypetcare.mypetcare package but was " + appContext.getPackageName(),
+            "org.pesmypetcare.mypetcare", appContext.getPackageName());
     }
 }
