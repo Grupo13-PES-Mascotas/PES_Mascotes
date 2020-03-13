976f68cf6edf9ebb09e371ae03bc75c0cb873c93
xavier-campos <xavier.campos.diaz@est.fib.upc.edu>
2020-03-13 13:02:47 +0100

    Restructure the login view

--- app/src/main/java/org/pesmypetcare/mypetcare/activities/LoginActivity.java
+++ app/src/main/java/org/pesmypetcare/mypetcare/activities/LoginActivity.java
@@ -1,12 +1,15 @@
 package org.pesmypetcare.mypetcare.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 import org.pesmypetcare.mypetcare.databinding.ActivityLoginBinding;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends FragmentActivity {
     private ActivityLoginBinding binding;
 
     @Override
