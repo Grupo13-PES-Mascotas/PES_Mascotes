35a3cca2560f4dca9d34a82dc2b93664f4fe1ee8
santidrj <59149491+santidrj@users.noreply.github.com>
2020-03-13 23:18:09 +0100

    Update decrypt_key.sh

--- .github/scripts/decrypt_key.sh
+++ .github/scripts/decrypt_key.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+chmod +x keystore.properties.gpg
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$RELEASE_PASSPHRASE" --output . keystore.properties.gpg
