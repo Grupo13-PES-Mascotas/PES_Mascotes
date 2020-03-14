163485cf5e03c77894d0aeb9ecf46ecd3dc7d9e1
Albert Pinto i Gil <albert.pinto@est.fib.upc.edu>
2020-03-14 16:33:27 +0100

    Add the profile text size
    The size of the profile has been set

--- app/src/main/java/org/pesmypetcare/mypetcare/activities/MainActivity.java
+++ app/src/main/java/org/pesmypetcare/mypetcare/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package org.pesmypetcare.mypetcare.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
@@ -8,11 +9,14 @@ import org.pesmypetcare.mypetcare.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        drawerLayout = binding.activityMainDrawerLayout;
     }
 }
