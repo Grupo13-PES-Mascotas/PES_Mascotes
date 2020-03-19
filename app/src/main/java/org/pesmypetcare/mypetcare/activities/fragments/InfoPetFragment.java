5e31d163c39e1a4038d786c0513c7839e82147e3
danicm47 <daniel.clemente.marin@est.fib.upc.edu>
2020-03-19 20:14:28 +0100

    Finish pet info fragment

--- app/src/main/java/org/pesmypetcare/mypetcare/activities/fragments/InfoPetFragment.java
+++ app/src/main/java/org/pesmypetcare/mypetcare/activities/fragments/InfoPetFragment.java
@@ -7,12 +7,53 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+
+
+import com.google.android.material.datepicker.MaterialDatePicker;
 
 import org.pesmypetcare.mypetcare.R;
+import org.pesmypetcare.mypetcare.databinding.FragmentInfoPetBinding;
+
+import java.util.Objects;
+
 
 public class InfoPetFragment extends Fragment {
+    private FragmentInfoPetBinding binding;
+    private MaterialDatePicker materialDatePicker;
+    private Button birthDate;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_info_pet, container, false);
+        binding = FragmentInfoPetBinding.inflate(inflater, container, false);
+        
+        setCalendarPicker();
+        setGenderDropdownMenu();
+
+        return binding.getRoot();
+    }
+
+    private void setGenderDropdownMenu() {
+        AutoCompleteTextView gender = binding.inputGender;
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(Objects.requireNonNull(getContext()),
+        R.layout.drop_down_menu_item, new String[] {getString(R.string.male), getString(R.string.female)});
+        gender.setAdapter(adapter);
+    }
+
+    private void setCalendarPicker() {
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText(getString(R.string.birth_random));
+        MaterialDatePicker materialDatePicker = builder.build();
+
+        birthDate = binding.inputBirthMonth;
+        birthDate.setOnClickListener(v ->
+                materialDatePicker.show(Objects.requireNonNull(getFragmentManager()), "DATE_PICKER"));
+
+        materialDatePicker.addOnPositiveButtonClickListener(selection -> {
+            birthDate.setText(materialDatePicker.getHeaderText());
+        });
     }
 }
