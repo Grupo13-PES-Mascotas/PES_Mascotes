41206b9d21ff659cf7a0ebed79cc77e037d128bd
enrichp <enrichernandopuerta@gmail.com>
2020-03-20 16:58:58 +0100

    Correct codacy issues and add comments

--- app/src/test/java/org/pesmypetcare/mypetcare/features/pets/TestPetUpdate.java
+++ app/src/test/java/org/pesmypetcare/mypetcare/features/pets/TestPetUpdate.java
@@ -25,37 +25,41 @@ public class TestPetUpdate {
         pet.setWashFrequency(2);
         pet.setWeight(2);
 
-        usr = new User ( "Oriol", "er@gmail.com", "909090");
-        usr.addPet(pet);
+        usr = new User("johnDoe", "johndoe@gmail.com", "1234");
         trUpdatePet = new TrUpdatePet(new StubPetManagerService());
     }
 
     @Test
     public void shouldChangeGender() {
+        usr.addPet(pet);
         pet.setGender(Gender.MALE);
         assertEquals(Gender.MALE, pet.getGender());
     }
 
     @Test
     public void shouldChangeWeight() {
+        usr.addPet(pet);
         pet.setWeight(30);
         assertEquals(30, pet.getWeight(),0);
     }
 
     @Test
     public void shouldChangeName() {
+        usr.addPet(pet);
         pet.setName("Ohio");
         assertEquals("Ohio", pet.getName());
     }
 
     @Test
     public void shouldChangeBreed() {
+        usr.addPet(pet);
         pet.setBreed("Bulldog");
         assertEquals("Bulldog", pet.getBreed());
     }
 
     @Test
     public void shouldUpdatePetService() throws PetAlreadyExistingException, UserIsNotOwnerException {
+        usr.addPet(pet);
         pet.setWeight(20);
         trUpdatePet.setUser(usr);
         trUpdatePet.setPet(pet);
@@ -68,6 +72,7 @@ public class TestPetUpdate {
 
     @Test(expected = UserIsNotOwnerException.class)
     public void shouldNotUpdatePetIfNotOwner() throws PetAlreadyExistingException, UserIsNotOwnerException {
+        usr.addPet(pet);
         User usr2 = new User ( "Gabi", "er2@gmail.com", "909020");
         trUpdatePet.setUser(usr2);
         trUpdatePet.setPet(pet);
@@ -76,6 +81,7 @@ public class TestPetUpdate {
 
     @Test(expected = PetAlreadyExistingException.class)
     public void shouldNotUpdatePetIfExisting() throws PetAlreadyExistingException, UserIsNotOwnerException {
+        usr.addPet(pet);
         Pet linux = getAuxPet();
         usr.addPet(linux);
         pet.setName("Linux");
