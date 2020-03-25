9356d30351d71ab38a5d7434f19fa08fba87a02d
Albert Pinto i Gil <albert.pinto@est.fib.upc.edu>
2020-03-25 16:10:45 +0100

    Test ImageZoom and fix a little issue

--- app/src/main/java/org/pesmypetcare/mypetcare/activities/fragments/MyPetsFragment.java
+++ app/src/main/java/org/pesmypetcare/mypetcare/activities/fragments/MyPetsFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import org.pesmypetcare.mypetcare.R;
+import org.pesmypetcare.mypetcare.activities.MainActivity;
 import org.pesmypetcare.mypetcare.activities.communication.MyPetsComunication;
 import org.pesmypetcare.mypetcare.databinding.FragmentMyPetsBinding;
 import org.pesmypetcare.mypetcare.features.pets.Pet;
@@ -59,6 +60,7 @@ public class MyPetsFragment extends Fragment {
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.mainActivityFrameLayout, testFragment);
                 ft.commit();
+                MainActivity.setActualFragment(testFragment);
             });
             ++index;
         }
