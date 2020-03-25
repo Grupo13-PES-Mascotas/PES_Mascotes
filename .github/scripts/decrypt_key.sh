07f36717b9d282a8820b2555d748371164363e7f
santidrj <59149491+santidrj@users.noreply.github.com>
2020-03-25 15:43:06 +0100

    Update decrypt_key.sh

--- .github/scripts/decrypt_key.sh
+++ .github/scripts/decrypt_key.sh
@@ -2,5 +2,5 @@
 
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
-# gpg --quiet --batch --yes --decrypt --passphrase="$RELEASE_PASSPHRASE" --output keystore.properties keystore.properties.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$RELEASE_PASSPHRASE" --output keystore.properties keystore.properties.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_PASSPHRASE" --output ./app/google-services.json ./app/google-services.json.gpg
