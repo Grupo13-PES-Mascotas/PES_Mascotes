b9c8de027907ed0be79151a4eb7157dc47c6925c
enrichp <enrichernandopuerta@gmail.com>
2020-04-15 11:44:34 +0200

    Codacy issues

--- app/src/test/java/org/pesmypetcare/mypetcare/controllers/TestTrExistsUsername.java
+++ app/src/test/java/org/pesmypetcare/mypetcare/controllers/TestTrExistsUsername.java
@@ -2,36 +2,34 @@ package org.pesmypetcare.mypetcare.controllers;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.pesmypetcare.mypetcare.features.users.User;
 import org.pesmypetcare.mypetcare.services.StubUserManagerService;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TestTrExistsUsername {
-    private User user;
     private TrExistsUsername trExistsUsername;
-    private final String USERNAME = "enric";
+    private final String USERNAME = "johnDoe";
 
     @Before
     public void setUp() {
-        user = new User("johnDoe", "johndoe@gmail.com", "ndekdme");
         trExistsUsername = new TrExistsUsername(new StubUserManagerService());
     }
 
     @Test
     public void shouldReturnFalse() {
-        trExistsUsername.setNewUsername(USERNAME);
+        trExistsUsername.setNewUsername("enric");
         trExistsUsername.execute();
         boolean addingResult = trExistsUsername.isResult();
-        assertFalse("should communicate with service to verify the username", addingResult);
+        assertFalse("should communicate with service to verify the username is not repeated",
+                addingResult);
     }
 
     @Test
     public void shouldReturnTrue() {
-        trExistsUsername.setNewUsername("johnDoe");
+        trExistsUsername.setNewUsername(USERNAME);
         trExistsUsername.execute();
         boolean addingResult = trExistsUsername.isResult();
-        assertTrue("should communicate with service to verify the username", addingResult);
+        assertTrue("should communicate with service to verify the username is repeated", addingResult);
     }
 }
