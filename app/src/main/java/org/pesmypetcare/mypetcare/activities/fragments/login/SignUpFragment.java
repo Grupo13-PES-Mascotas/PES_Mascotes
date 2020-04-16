b01e07538564ffc0573d58a8df98c0bc804608e5
enrichp <enrichernandopuerta@gmail.com>
2020-04-16 23:52:06 +0200

    New branch signup Google

--- app/src/main/java/org/pesmypetcare/mypetcare/activities/fragments/login/SignUpFragment.java
+++ app/src/main/java/org/pesmypetcare/mypetcare/activities/fragments/login/SignUpFragment.java
@@ -14,9 +14,17 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import org.pesmypetcare.mypetcare.R;
 import org.pesmypetcare.mypetcare.activities.MainActivity;
@@ -59,8 +67,48 @@ public class SignUpFragment extends Fragment {
             }
             resetFieldsStatus();
         });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(Objects.requireNonNull(getActivity()), gso);
+        binding.signupGoogleButton.setOnClickListener(v -> {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, 9001);
+        });
+
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 9001) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                assert account != null;
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException | ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) throws ExecutionException, InterruptedException {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(Objects.requireNonNull(getActivity()), task -> {
+                    if (task.isSuccessful()) {
+                        userManagerService.createUser(Objects.requireNonNull(mAuth.getCurrentUser()).getUid(),
+                                acct.getDisplayName(), acct.getEmail(), "");
+                        startActivity(new Intent(getActivity(), MainActivity.class));
+                        Objects.requireNonNull(getActivity()).finish();
+                    }
+                });
+
+    }
+
 
     /**
      * This method declares the editText and InputLayout.
@@ -81,7 +129,8 @@ public class SignUpFragment extends Fragment {
                     .addOnCompleteListener(Objects.requireNonNull(getActivity()), task -> {
                         if (task.isSuccessful()) {
                             sendEmailVerification();
-                            userManagerService.createUser(Objects.requireNonNull(mAuth.getCurrentUser()).getUid(), username, email, password);
+                            userManagerService.createUser(Objects.requireNonNull(mAuth.getCurrentUser()).getUid(),
+                                    username, email, password);
                             mAuth.signOut();
                         } else {
                             testToast(Objects.requireNonNull(task.getException()).toString());
